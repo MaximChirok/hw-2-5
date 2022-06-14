6b4f5e10038b30cfeecc49d2b1662ab63d2362a2
Chirkov Maxim <maxim.chirkov@skyeng.ru>
2022-06-15 00:03:54 +0700

    1-try

---
+++ src/main/java/pro/sky/hw_2_5/service/EmployeeService.java
@@ -0,0 +1,58 @@
+package pro.sky.hw_2_5.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.hw_2_5.exceptions.EmployeeAlreadyAddedException;
+import pro.sky.hw_2_5.exceptions.EmployeeNotFoundException;
+import pro.sky.hw_2_5.exceptions.EmployeeStorageIsFullException;
+import pro.sky.hw_2_5.model.Employee;
+
+import java.util.*;
+
+@Service
+public class EmployeeService {
+
+    private static final int LIMIT = 10;
+
+    private final List<Employee> employees = new ArrayList<>(10);
+
+    public Employee add(String name, String surname) {
+        Employee employee = new Employee(name, surname);
+        if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        }
+
+        if (employees.size()<LIMIT) {
+            employees.add(employee);
+            return employee;
+        }
+        throw new EmployeeStorageIsFullException();
+
+    }
+
+
+    public Employee remove(String name, String surname) {
+        Employee employee = new Employee(name, surname);
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        employees.remove(employee);
+        return employee;
+    }
+
+
+    public Employee find(String name, String surname) {
+        Employee employee = new Employee(name, surname);
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+                return employee;
+            }
+
+
+
+    public List<Employee> getAll() {
+       return new ArrayList<>(employees);
+    }
+
+
+}
